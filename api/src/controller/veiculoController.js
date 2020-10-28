a9e9528454688f1041162dfab804d4aefa947198
Joao Bragança <jvbragancaa@gmail.com>
2020-10-28 12:07:43 -0300

    Documentação dos métodos

--- api/src/controller/veiculoController.js
+++ api/src/controller/veiculoController.js
@@ -5,7 +5,7 @@ const Veiculo = require("../models/Veiculo");
 const router = express.Router();
 
 router.get("/lastId", async (req, res) => {
-  await Veiculo.findOne().sort({ _id: -1}).exec((err, data) => {
+  await Veiculo.findOne().sort({_id: -1}).exec((err, data) => {
     return res.send(data._id);
   })
 });
@@ -13,47 +13,50 @@ router.get("/lastId", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const veiculo = await Veiculo.create(req.body);
-    res.json({ veiculo });
+    res.json({veiculo});
   } catch (error) {
-    return res.json({ message: error });
+    return res.json({message: error});
   }
 });
 
+// Retorna todos os veículos
 router.get("/", async (req, res) => {
   await Veiculo.find({}, (err, veiculos) => {
     if (!err) return res.json(veiculos);
   });
 });
 
+// Retorna um objeto com o veículo que foi bsucado de acordo com a ID passada
 router.get("/:veiculoId", async (req, res) => {
   try {
     const veiculo = await Veiculo.findById(req.params.veiculoId);
     return res.json(veiculo);
   } catch (error) {
-    return res.json({ message: error });
+    return res.json({message: error});
   }
 });
 
+// Deleta o veículo com a ID passada
 router.delete("/:veiculoId", async (req, res) => {
   try {
-    const removed = await Veiculo.deleteOne({ _id: req.params.veiculoId });
+    const removed = await Veiculo.deleteOne({_id: req.params.veiculoId});
     return res.json(removed);
   } catch (error) {
-    return res.json({ message: error });
+    return res.json({message: error});
   }
 });
 
+// Altera os dados passados do veículo desejado
 router.patch("/:veiculoId", async (req, res) => {
   try {
     const updated = await Veiculo.updateOne(
-      { _id: req.params.veiculoId },
+      {_id: req.params.veiculoId},
       req.body
     );
     return res.json(updated);
   } catch (error) {
-    return res.json({ message: error });
+    return res.json({message: error});
   }
 });
-
 
 module.exports = (app) => app.use("/api/v1", router);
--- api/src/database/index.js
+++ api/src/database/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+// URL de conexão está no arquivo .env
 mongoose.connect(process.env.DB_CONNECTION, {
   useNewUrlParser: true,
   useCreateIndex: true,
